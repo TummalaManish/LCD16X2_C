a141ade5ca11be82546aeb6d47303c229629f69d
tummalamanish <manishthumala12@gmail.com>
2020-04-08 17:15:16 +0530

    Page and Line structures(internal implementations) have been added.
    Test folder and test.c has been added for testing the Library.

---
+++ LCD16X2_C/Include/LCD16X2_C_i.h
@@ -0,0 +1,47 @@
+/**
+ * @file LCD16X2_c.h
+ * @author ManishTummala
+ * @date 9 TODO
+ * @brief Internal interface header for the LCD16X2 library.
+ *
+ * TODO:
+ *
+ * @see http://www.stack.nl/~dimitri/doxygen/docblocks.html
+ * @see http://www.stack.nl/~dimitri/doxygen/commands.html
+ * https://www.google.com
+ */
+
+/*!
+ * general naming style and information
+ *
+ * Only typedef of the structures are encouraged to be used in the application
+ * every structure typedef will have a '_s' post-fix.
+ * all the structures are not exposed to the application and cannot be used in that way.
+ *
+ */
+#ifndef INCLUDE_LCD16X2_C_I_H_
+#define INCLUDE_LCD16X2_C_I_H_
+
+/*Dependent Includes*/
+#include <stdint.h>
+
+typedef struct Line_ll{/*Line_ll is used only inside the library*/
+	/*Main Elements of the structure*/
+	char current_char_; /*<Place holder for each character in the line>*/
+
+	/*Links to the other elements on the Linked List*/
+	char *forward_, *backward_; /*<Pointers to the other characters in the line>*/
+}line_s;
+
+typedef struct Page_ll{/*Page_ll is used only inside the library*/
+	/*Main Elements of the structure*/
+	/*TODO: Data type decelerations must be modifiable by preprocessor directives*/
+	uint8_t current_line_numebr; /*<Holds the number of current line in the Page>*/
+	struct Line_ll current_page_; /*<Current line element that can be accessed as an structure element>*/
+
+	/*Links to the other elements in the Linked List*/
+	struct Line_ll* forward_, backward_; /*<Pointers to the other lines in the page>*/
+}page_s;
+
+
+#endif /* INCLUDE_LCD16X2_C_I_H_ */
---
+++ LCD16X2_C/Tests/tests.c
@@ -0,0 +1,32 @@
+/**
+ * @file tests.c
+ * @author ManishTummala
+ * @date TODO
+ * @brief This file contains the tests for the library.
+ *
+ * TODO:
+ *
+ *
+ * @see http://www.stack.nl/~dimitri/doxygen/docblocks.html
+ * @see http://www.stack.nl/~dimitri/doxygen/commands.html
+ * https://www.google.com
+ */
+
+/*--------------- Library Includes --------------*/
+#include "LCD16X2_C_i.h"
+/*-----------------------------------------------*/
+
+/*----------------- Test values ---=-------------*/
+#define TEST_FAIL  0
+/*-----------------------------------------------*/
+
+/*------ Test Definitions and Deceleration ------*/
+
+/*-----------------------------------------------*/
+
+
+int main(){ /*<Entry point for the test code*/
+
+	int TEST_EXIT_CODE = TEST_FAIL; /*<Varible updated by the tests that indicates test state.>*/
+	return TEST_EXIT_CODE;
+}
